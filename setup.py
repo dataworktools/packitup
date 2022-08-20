00e1d471e97f842c62908c0ce724dc958f21310e
devizn-data <111545041+devizn-data@users.noreply.github.com>
2022-08-19 22:02:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='toolbox',
-    version='0.0.3',
+    version='0.0.1',
     author='Felix A. Sanchez',
     author_email='devizn-data@gmail.com',
     description='Testing installation of Package',
