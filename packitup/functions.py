17e06a56f2f34cad518a7e45edfa3ce2d8837d44
Felix A. Sanchez <111545041+devizn-data@users.noreply.github.com>
2022-08-20 21:52:41 -0400

    Update functions.py

--- packitup/functions.py
+++ packitup/functions.py
@@ -1,24 +1,24 @@
 # importing requests package
 import requests	
 
-def NewsFromBBC():
+def ESGNews():
 	
-	# BBC news api
+	# ESG news api
 	# following query parameters are used
 	# source, sortBy and apiKey
-	query_params = {
-	"source": "bbc-news",
-	"sortBy": "top",
-	"apiKey": "bcc83430e9964c40ab973b1012bd82cb"
-	}
-	main_url = " https://newsapi.org/v1/articles"
-
+	url = ('https://newsapi.org/v2/everything?'
+       'q=ESG&'
+       'language=en&'
+       'from=2022-08-20&'
+       'sortBy=relevancy&'
+       'apiKey=bcc83430e9964c40ab973b1012bd82cb')
+	    
 	# fetching data in json format
-	res = requests.get(main_url, params=query_params)
-	open_bbc_page = res.json()
+	response = requests.get(url)
+	open_ESG_page = response.json()
 
 	# getting all articles in a string article
-	article = open_bbc_page["articles"]
+	article = open_ESG_page["articles"]
 
 	# empty list which will
 	# contain all trending news
@@ -26,19 +26,17 @@ def NewsFromBBC():
 	
 	for ar in article:
 		results.append(ar["title"])
-		
+		results.append(ar["description"])
+		results.append(ar["source"])
+		results.append(ar["url"])
+	
 	for i in range(len(results)):
 		
 		# printing all trending news
 		print(i + 1, results[i])
 
-	#to read the news out loud for us
-	from win32com.client import Dispatch
-	speak = Dispatch("SAPI.Spvoice")
-	speak.Speak(results)				
-
 # Driver Code
 if __name__ == '__main__':
 	
 	# function call
-	NewsFromBBC()
+	ESGNews()
